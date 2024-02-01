61a69e09e8120d9f74e4e7ba0a54b75ec68d9ee9
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-02-01 13:10:43 +0530

    installed and initialized the cloudinary

---
+++ server/src/cloudinary/cloudinary.ts
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "drbqyvmbj",
+  api_key: "846438861125287",
+  api_secret: "Yh8UFngVVY4BLvvM_asQVtV0XXY",
+});
+
+module.exports = cloudinary;
